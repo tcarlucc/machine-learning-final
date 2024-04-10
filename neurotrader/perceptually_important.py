33dab87e544e638c2c28e5842ecc89051ddde54b
Nick-Altland <naltland@uvm.edu>
2024-04-10 10:21:31 -0400

    Created reversal_patterns.ipynb

--- neurotrader/perceptually_important.py
+++ neurotrader/perceptually_important.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 import mplfinance as mpf
 import yfinance as yf
 
+
 def find_pips(data: np.array, n_pips: int, dist_measure: int):
     # dist_measure
     # 1 = Euclidean Distance
     # 2 = Perpindicular Distance
     # 3 = Vertical Distance
     pips_x = [0, len(data) - 1]  # Index
-    pips_y = [data[0], data[-1]] # Price
+    pips_y = [data[0], data[-1]]  # Price
 
     for curr_point in range(2, n_pips):
 
-        md = 0.0 # Max distance
-        md_i = -1 # Max distance index
+        md = 0.0  # Max distance
+        md_i = -1  # Max distance index
         insert_index = -1
 
         for k in range(0, curr_point - 1):
@@ -30,15 +31,15 @@ def find_pips(data: np.array, n_pips: int, dist_measure: int):
             intercept = pips_y[left_adj] - pips_x[left_adj] * slope;
 
             for i in range(pips_x[left_adj] + 1, pips_x[right_adj]):
-                
-                d = 0.0 # Distance
-                if dist_measure == 1: # Euclidean distance
-                    d =  ( (pips_x[left_adj] - i) ** 2 + (pips_y[left_adj] - data[i]) ** 2 ) ** 0.5
-                    d += ( (pips_x[right_adj] - i) ** 2 + (pips_y[right_adj] - data[i]) ** 2 ) ** 0.5
-                elif dist_measure == 2: # Perpindicular distance
-                    d = abs( (slope * i + intercept) - data[i] ) / (slope ** 2 + 1) ** 0.5
-                else: # Vertical distance    
-                    d = abs( (slope * i + intercept) - data[i] )
+
+                d = 0.0  # Distance
+                if dist_measure == 1:  # Euclidean distance
+                    d = ((pips_x[left_adj] - i) ** 2 + (pips_y[left_adj] - data[i]) ** 2) ** 0.5
+                    d += ((pips_x[right_adj] - i) ** 2 + (pips_y[right_adj] - data[i]) ** 2) ** 0.5
+                elif dist_measure == 2:  # Perpindicular distance
+                    d = abs((slope * i + intercept) - data[i]) / (slope ** 2 + 1) ** 0.5
+                else:  # Vertical distance
+                    d = abs((slope * i + intercept) - data[i])
 
                 if d > md:
                     md = d
