a8f1fc252ad4706a35662642adb1cc1a7e1ebd96
Nick <naltland@uvm.edu>
2024-04-17 16:30:52 -0400

    Edited harmonic patterns, adding comments

--- neurotrader/harmonic_patterns.py
+++ neurotrader/harmonic_patterns.py
@@ -14,6 +14,7 @@ from dataclasses import dataclass
 from typing import Union
 from math import log
 
+
 # This holds the ratio of the patterns
 # Can be three types: Float, List, or None
 @dataclass
@@ -24,12 +25,13 @@ class XABCD:
     XA_AD: Union[float, list, None]
     name: str
 
+
 # Define Patterns
 # Notice that there is a range of acceptable values
 # Pattern recognition can handle any combination as some patterns do not need all values
 GARTLEY = XABCD(0.618, [0.382, 0.886], [1.13, 1.618], 0.786, "Gartley")
 BAT = XABCD([0.382, 0.50], [0.382, 0.886], [1.618, 2.618], 0.886, "Bat")
-#ALT_BAT = XABCD(0.382, [0.382, 0.886], [2.0, 3.618], 1.13, "Alt Bat")
+# ALT_BAT = XABCD(0.382, [0.382, 0.886], [2.0, 3.618], 1.13, "Alt Bat")
 BUTTERFLY = XABCD(0.786, [0.382, 0.886], [1.618, 2.24], [1.27, 1.41], "Butterfly")
 CRAB = XABCD([0.382, 0.618], [0.382, 0.886], [2.618, 3.618], 1.618, "Crab")
 DEEP_CRAB = XABCD(0.886, [0.382, 0.886], [2.0, 3.618], 1.618, "Deep Crab")
@@ -38,6 +40,7 @@ SHARK = XABCD(None, [1.13, 1.618], [1.618, 2.24], [0.886, 1.13], "Shark")
 # Have to add all the patterns to this list
 ALL_PATTERNS = [GARTLEY, BAT, BUTTERFLY, CRAB, DEEP_CRAB, CYPHER, SHARK]
 
+
 # This holds the location of all the found patterns
 @dataclass
 class XABCDFound:
@@ -45,10 +48,11 @@ class XABCDFound:
     A: int
     B: int
     C: int
-    D: int # Index of last point in pattern, the entry is on the close of D
-    error: float # Error found
+    D: int  # Index of last point in pattern, the entry is on the close of D
+    error: float  # Error found
     name: str
     bull: bool
+
 
 # Plots the patterns once we find them
 def plot_pattern(ohlc: pd.DataFrame, pat: XABCDFound, pad=3):
@@ -65,13 +69,12 @@ def plot_pattern(ohlc: pd.DataFrame, pat: XABCDFound, pad=3):
     else:
         s2 = ohlc['low'].to_numpy()
         s1 = ohlc['high'].to_numpy()
-        
 
     l0 = [(idx[pat.X], s1[pat.X]), (idx[pat.A], s2[pat.A])]
     l1 = [(idx[pat.A], s2[pat.A]), (idx[pat.B], s1[pat.B])]
     l2 = [(idx[pat.B], s1[pat.B]), (idx[pat.C], s2[pat.C])]
     l3 = [(idx[pat.C], s2[pat.C]), (idx[pat.D], s1[pat.D])]
-    
+
     # Connecting lines
     l4 = [(idx[pat.A], s2[pat.A]), (idx[pat.C], s2[pat.C])]
     l5 = [(idx[pat.B], s1[pat.B]), (idx[pat.D], s1[pat.D])]
@@ -79,49 +82,56 @@ def plot_pattern(ohlc: pd.DataFrame, pat: XABCDFound, pad=3):
     l7 = [(idx[pat.X], s1[pat.X]), (idx[pat.D], s1[pat.D])]
 
     mpf.plot(
-        data, 
-        alines=dict(alines=[l0, l1, l2, l3, l4, l5, l6, l7 ], colors=['w', 'w', 'w', 'w', 'b', 'b', 'b', 'b']),
+        data,
+        alines=dict(alines=[l0, l1, l2, l3, l4, l5, l6, l7], colors=['w', 'w', 'w', 'w', 'b', 'b', 'b', 'b']),
         type='candle', style='charles', ax=ax
     )
-    
+
     # Text
-    xa_ab =  abs(s2[pat.A] - s1[pat.B]) / abs(s1[pat.X] - s2[pat.A])
-    ab_bc =  abs(s1[pat.B] - s2[pat.C]) / abs(s2[pat.A] - s1[pat.B])
-    bc_cd =  abs(s2[pat.C] - s1[pat.D]) / abs(s1[pat.B] - s2[pat.C])
-    xa_ad =  abs(s2[pat.A] - s1[pat.D]) / abs(s1[pat.X] - s2[pat.A])
-    ax.text(int((pat.X + pat.B) / 2) - pat.X + pad, (s1[pat.X] + s1[pat.B]) / 2 , str(round(xa_ab, 3)), color='orange', fontsize='x-large')
-    ax.text(int((pat.A + pat.C) / 2) - pat.X + pad, (s2[pat.A] + s2[pat.C]) / 2 , str(round(ab_bc, 3)), color='orange', fontsize='x-large')
-    ax.text(int((pat.B + pat.D) / 2) - pat.X + pad, (s1[pat.B] + s1[pat.D]) / 2 , str(round(bc_cd, 3)), color='orange', fontsize='x-large')
-    ax.text(int((pat.X + pat.D) / 2) - pat.X + pad, (s1[pat.X] + s1[pat.D]) / 2 , str(round(xa_ad, 3)), color='orange', fontsize='x-large')
-    
+    xa_ab = abs(s2[pat.A] - s1[pat.B]) / abs(s1[pat.X] - s2[pat.A])
+    ab_bc = abs(s1[pat.B] - s2[pat.C]) / abs(s2[pat.A] - s1[pat.B])
+    bc_cd = abs(s2[pat.C] - s1[pat.D]) / abs(s1[pat.B] - s2[pat.C])
+    xa_ad = abs(s2[pat.A] - s1[pat.D]) / abs(s1[pat.X] - s2[pat.A])
+    ax.text(int((pat.X + pat.B) / 2) - pat.X + pad, (s1[pat.X] + s1[pat.B]) / 2, str(round(xa_ab, 3)), color='orange',
+            fontsize='x-large')
+    ax.text(int((pat.A + pat.C) / 2) - pat.X + pad, (s2[pat.A] + s2[pat.C]) / 2, str(round(ab_bc, 3)), color='orange',
+            fontsize='x-large')
+    ax.text(int((pat.B + pat.D) / 2) - pat.X + pad, (s1[pat.B] + s1[pat.D]) / 2, str(round(bc_cd, 3)), color='orange',
+            fontsize='x-large')
+    ax.text(int((pat.X + pat.D) / 2) - pat.X + pad, (s1[pat.X] + s1[pat.D]) / 2, str(round(xa_ad, 3)), color='orange',
+            fontsize='x-large')
+
     desc_string = pat.name
-    desc_string += "\nError: " + str(round(pat.error , 5))
+    desc_string += "\nError: " + str(round(pat.error, 5))
     if pat.bull:
         plt_price = data['high'].max() - 0.05 * (data['high'].max() - data['low'].min())
     else:
         plt_price = data['low'].min() + 0.05 * (data['high'].max() - data['low'].min())
-    ax.text(0, plt_price , desc_string, color='yellow', fontsize='x-large')
+    ax.text(0, plt_price, desc_string, color='yellow', fontsize='x-large')
     plt.show()
 
+# COMPARES Retracement ratios
+# Actual ratio: What we observe
+# Pattern Ratio: The Ideal ratio for that pattern
 def get_error(actual_ratio: float, pattern_ratio: Union[float, list, None]):
-     
-    if pattern_ratio is None: # No requirement (Shark)
+    if pattern_ratio is None:  # No requirement (Shark)
         return 0.0
 
     log_actual = log(actual_ratio)
 
-    if isinstance(pattern_ratio, list): # Acceptable range
+    if isinstance(pattern_ratio, list):  # Acceptable range
         log_pat0 = log(pattern_ratio[0])
         log_pat1 = log(pattern_ratio[1])
-        assert(log_pat1 > log_pat0)
+        assert (log_pat1 > log_pat0)
 
+        # Makes sure the value is in the range
         if log_pat0 <= log_actual <= log_pat1:
             return 0.0
-        #else:
+        # else:
         #    return 1e20
 
-        err = min( abs(log_actual - log_pat0), abs(log_actual - log_pat1) )
-        range_mult = 2.0 # Since range is already more lenient, punish harder. 
+        err = min(abs(log_actual - log_pat0), abs(log_actual - log_pat1))
+        range_mult = 2.0  # Since range is already more lenient, punish harder.
         err *= range_mult
         return err
 
@@ -132,11 +142,14 @@ def get_error(actual_ratio: float, pattern_ratio: Union[float, list, None]):
         raise TypeError("Invalid pattern ratio type")
 
 
+# This is where we look for the pattern types from above
 def find_xabcd(ohlc: pd.DataFrame, extremes: pd.DataFrame, err_thresh: float = 0.2):
-    
+    # Get the retracement values (see retracement_ratios.py
     extremes['seg_height'] = (extremes['ext_p'] - extremes['ext_p'].shift(1)).abs()
-    extremes['retrace_ratio'] = extremes['seg_height'] / extremes['seg_height'].shift(1) 
-    
+    extremes['retrace_ratio'] = extremes['seg_height'] / extremes['seg_height'].shift(1)
+
+    # Prepare the output
+    # For each output create two signals concurrent with the candle data
     output = {}
     for pat in ALL_PATTERNS:
         pat_data = {}
@@ -145,36 +158,42 @@ def find_xabcd(ohlc: pd.DataFrame, extremes: pd.DataFrame, err_thresh: float = 0
         pat_data['bear_signal'] = np.zeros(len(ohlc))
         pat_data['bear_patterns'] = []
         output[pat.name] = pat_data
-    
+
+    # Index of first confirmed directional change. Start main loop here
     first_conf = extremes.index[0]
+    # Index of first confirmed top or bottom
     extreme_i = 0
-        
+
+    # Controls the trades
+    # TODO: Here is where we could implement a Neural Network?
+    # TODO: Change the weight here of when to make a trade
     entry_taken = 0
     pattern_used = None
+
     for i in range(first_conf, len(ohlc)):
-        
+
         if extremes.index[extreme_i + 1] == i:
             entry_taken = 0
             extreme_i += 1
-        
+
         if entry_taken != 0:
             if entry_taken == 1:
                 output[pattern_used]['bull_signal'][i] = 1
             else:
                 output[pattern_used]['bear_signal'][i] = -1
             continue
-        
+
         if extreme_i + 1 >= len(extremes):
             break
-        
+
         if extreme_i < 3:
             continue
 
         ext_type = extremes.iloc[extreme_i]['type']
         last_conf_i = extremes.index[extreme_i]
 
-        
-        if extremes.iloc[extreme_i]['type'] > 0.0:  
+        # Get price of current high or low
+        if extremes.iloc[extreme_i]['type'] > 0.0:
             # Last extreme was a top, meaning we're on a leg down currently.
             # We are checking for bull patterns
             D_price = ohlc.iloc[i]['low']
@@ -189,11 +208,11 @@ def find_xabcd(ohlc: pd.DataFrame, extremes: pd.DataFrame, err_thresh: float = 0
             if ohlc.iloc[last_conf_i:i]['high'].max() > D_price:
                 continue
 
-         
         # D_Price set, get ratios
-        dc_retrace = abs(D_price - extremes.iloc[extreme_i]['ext_p']) / extremes.iloc[extreme_i]['seg_height'] 
-        xa_ad_retrace = abs(D_price - extremes.iloc[extreme_i - 2]['ext_p']) / extremes.iloc[extreme_i - 2]['seg_height']
-        
+        dc_retrace = abs(D_price - extremes.iloc[extreme_i]['ext_p']) / extremes.iloc[extreme_i]['seg_height']
+        xa_ad_retrace = abs(D_price - extremes.iloc[extreme_i - 2]['ext_p']) / extremes.iloc[extreme_i - 2][
+            'seg_height']
+
         best_err = 1e30
         best_pat = None
         for pat in ALL_PATTERNS:
@@ -205,15 +224,15 @@ def find_xabcd(ohlc: pd.DataFrame, extremes: pd.DataFrame, err_thresh: float = 0
             if err < best_err:
                 best_err = err
                 best_pat = pat.name
-        
+
         if best_err <= err_thresh:
             pattern_data = XABCDFound(
-                    int(extremes.iloc[extreme_i - 3]['ext_i']), 
-                    int(extremes.iloc[extreme_i - 2]['ext_i']), 
-                    int(extremes.iloc[extreme_i - 1]['ext_i']), 
-                    int(extremes.iloc[extreme_i]['ext_i']), 
-                    i, 
-                    best_err, best_pat, True
+                int(extremes.iloc[extreme_i - 3]['ext_i']),
+                int(extremes.iloc[extreme_i - 2]['ext_i']),
+                int(extremes.iloc[extreme_i - 1]['ext_i']),
+                int(extremes.iloc[extreme_i]['ext_i']),
+                i,
+                best_err, best_pat, True
             )
 
             pattern_used = best_pat
@@ -237,16 +256,17 @@ if __name__ == '__main__':
     data = pd.read_csv('BTCUSDT3600.csv')
     data['date'] = data['date'].astype('datetime64[s]')
     data = data.set_index('date')
-    #data = data[data.index < '2019-01-01']
-   
+    # data = data[data.index < '2019-01-01']
+
     # This takes a while to run fyi
     data['r'] = np.log(data['close']).diff().shift(-1)
     all_combined = np.zeros(len(data))
     sigmas = [0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04]
     for sigma in sigmas:
 
+        # Error threshold can be 0.2-0.5
         extremes = get_extremes(data, sigma)
-        output =  find_xabcd(data, extremes, 0.5)
+        output = find_xabcd(data, extremes, 0.5)
         sig = np.zeros(len(data))
         for pat in ALL_PATTERNS:
             sig += output[pat.name]['bear_signal'] + output[pat.name]['bull_signal']
@@ -256,11 +276,10 @@ if __name__ == '__main__':
     all_combined /= len(sigmas)
     data['combined_signal'] = all_combined
     data['combined_returns'] = data['r'] * data['combined_signal']
-    win_returns = data[data['combined_returns'] > 0]['combined_returns'].sum() 
-    lose_returns = data[data['combined_returns'] < 0]['combined_returns'].abs().sum() 
+    win_returns = data[data['combined_returns'] > 0]['combined_returns'].sum()
+    lose_returns = data[data['combined_returns'] < 0]['combined_returns'].abs().sum()
     combined_pf = win_returns / lose_returns
     print("Combined PF", combined_pf)
-    
 
     '''
     # Test single set of parameters
@@ -276,7 +295,7 @@ if __name__ == '__main__':
     plt.style.use('dark_background')
     data['signal_return'].cumsum().plot()
     '''
-    
+
     '''
     # Test several sigma vvalues
     data['r'] = np.log(data['close']).diff().shift(-1)
@@ -296,7 +315,7 @@ if __name__ == '__main__':
     plt.legend(prop={'size': 16})
     plt.show()
     '''
-    
+
     '''
     # Render error graph
     all_pfs = []
@@ -328,7 +347,6 @@ if __name__ == '__main__':
     plt.axhline(1.0, color='white')
     plt.show()
     '''
-    
 
     '''
     # Find best err pattern
@@ -389,5 +407,3 @@ if __name__ == '__main__':
     plt.legend(prop={'size': 16}, title='Sigma Values')
     plt.show()
     '''
-    
-
